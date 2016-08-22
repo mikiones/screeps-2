81295292fa84fda25ad08007df19f3227ec9b0cf
Moghedrin <nolat301@gmail.com>
2016-08-22 09:54:43 -0700

    Tutorial simple harvester followed

--- screep_scripts/main.js
+++ screep_scripts/main.js
@@ -1,2 +1,9 @@
+var roleHarvester = require('role.harvester.js');
+
 module.exports.loop = function () {
+
+	for(var name in Game.creeps) {
+		var creep = Game.creeps[name];
+		roleHarvester.run(creep);
+	}
 }
