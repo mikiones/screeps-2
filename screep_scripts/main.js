eb0819661996f214c285aa417682e68d743e707b
Moghedrin <nolat301@gmail.com>
2016-08-22 10:31:19 -0700

    Added spawn_screep function

--- screep_scripts/main.js
+++ screep_scripts/main.js
@@ -1,5 +1,18 @@
 var roleHarvester = require('role.simple_harvester');
 
+function make_id() {
+	var text = "";
+	var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	for( var i=0; i < 8; i++ ) {
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
+	}
+	return text;
+}
+
+function spawn_screep() {
+	Game.spawns['Spawn1'].createCreep( [WORK, CARRY, MOVE], 'Harvester'.concat(make_id()) );
+}
+
 module.exports.loop = function () {
 
 	for(var name in Game.creeps) {
