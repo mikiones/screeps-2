978a7128b7913d62cc4c856beb35049d2dd90224
Moghedrin <nolat301@gmail.com>
2016-08-22 10:24:00 -0700

    Find closest to screep

--- screep_scripts/role.simple_harvester.js
+++ screep_scripts/role.simple_harvester.js
@@ -3,7 +3,7 @@ var roleHarvester = {
 	/** @param {Creep} creep **/
 	run: function(creep) {
 		if(creep.carry.energy < creep.carryCapacity) {
-			var sources = creep.room.findClosestByRange(FIND_SOURCES);
+			var sources = creep.pos.findClosestByRange(FIND_SOURCES);
 			if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
 				creep.moveTo(sources[0]);
 			}
