52e6fd00b29c3743f600fa46f2e27601fd5f0e90
Moghedrin <nolat301@gmail.com>
2016-08-22 16:18:44 -0700

    Analyze sources if not already analyzed

--- screep_scripts/main.js
+++ screep_scripts/main.js
@@ -1,9 +1,13 @@
 var roleHarvester = require('role.simple_harvester');
 var roleUpgrader = require('role.simple_upgrader');
+var roleSpawner = require('role.spawner');
 var util = require('util');
 
 module.exports.loop = function () {
 	var spawner = Game.spawns['Spawn1'];
+	if (!spawner.memory.mining_info) {
+		roleSpawner.analyze_sources(spawner);
+	}
 	if (spawner.energy >= 300 && Object.keys(Game.creeps).length < 10) {
 		if (spawner.memory.upgrader_active) {
 			roleHarvester.spawn(spawner);
