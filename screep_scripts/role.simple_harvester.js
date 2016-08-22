ff683e0799cea397aeb4b1fcbe2e0212c2263f60
Moghedrin <nolat301@gmail.com>
2016-08-22 10:27:48 -0700

    Properly set screebs to go to nearest source (by linear distance. Flawed, at current.

--- screep_scripts/role.simple_harvester.js
+++ screep_scripts/role.simple_harvester.js
@@ -3,9 +3,9 @@ var roleHarvester = {
 	/** @param {Creep} creep **/
 	run: function(creep) {
 		if(creep.carry.energy < creep.carryCapacity) {
-			var sources = creep.room.find(FIND_SOURCES);
-			if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-				creep.moveTo(sources[0]);
+			var nearest = creep.pos.findClosestByRange(FIND_SOURCES);
+			if(creep.harvest(nearest) == ERR_NOT_IN_RANGE) {
+				creep.moveTo(nearest);
 			}
 		}
 		else if(Game.spawns['Spawn1'].energy < Game.spawns['Spawn1'].energyCapacity) {
