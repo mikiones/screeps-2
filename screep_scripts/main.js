efc991eb5d5c32f6fccc339a9509972a4d84b419
Moghedrin <nolat301@gmail.com>
2016-08-22 12:12:55 -0700

    Add spawn limit for creeps

--- screep_scripts/main.js
+++ screep_scripts/main.js
@@ -2,7 +2,7 @@ var roleHarvester = require('role.simple_harvester');
 var roleUpgrader = require('role.simple_upgrader');
 
 module.exports.loop = function () {
-	if (Game.spawns['Spawn1'].energy >= 300) {
+	if (Game.spawns['Spawn1'].energy >= 300 && Object.keys(Game.creeps).length < 10) {
 		roleHarvester.spawn(Game.spawns['Spawn1']);
 	}
 	for(var name in Game.creeps) {
