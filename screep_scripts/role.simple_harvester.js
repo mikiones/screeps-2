6766bff4faaf9dcd538e71801a32bfadf8815f75
Moghedrin <nolat301@gmail.com>
2016-08-22 12:55:58 -0700

    When harvesters deliver energy, they repick which source to mine from

--- screep_scripts/role.simple_harvester.js
+++ screep_scripts/role.simple_harvester.js
@@ -19,6 +19,8 @@ var roleHarvester = {
 		else if(Game.spawns['Spawn1'].energy < Game.spawns['Spawn1'].energyCapacity) {
 			if(creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
 				creep.moveTo(Game.spawns['Spawn1']);
+			} else {
+				creep.memory.harvest_src = undefined;
 			}
 		}
 	},
