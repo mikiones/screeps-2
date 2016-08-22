c135c96919d7d2c2c6557f662e992448d7eea999
Moghedrin <nolat301@gmail.com>
2016-08-22 13:49:26 -0700

    Harvesters should not longer just sit when spawn is at capacity

--- screep_scripts/role.simple_harvester.js
+++ screep_scripts/role.simple_harvester.js
@@ -15,8 +15,8 @@ var roleHarvester = {
 			if(creep.harvest(harvest_src) == ERR_NOT_IN_RANGE) {
 				creep.moveTo(harvest_src);
 			}
-		}
-		else if(Game.spawns['Spawn1'].energy < Game.spawns['Spawn1'].energyCapacity) {
+		} else {
+		//else if(Game.spawns['Spawn1'].energy < Game.spawns['Spawn1'].energyCapacity) {
 			if(creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
 				creep.moveTo(Game.spawns['Spawn1']);
 			} else {
