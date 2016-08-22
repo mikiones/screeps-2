addaac1258f8803fc6a6a8e3f8abf1b812ea8a74
Moghedrin <nolat301@gmail.com>
2016-08-22 14:21:28 -0700

    spawn harvester twice as often as upgrader

--- screep_scripts/main.js
+++ screep_scripts/main.js
@@ -4,7 +4,7 @@ var util = require('util');
 
 module.exports.loop = function () {
 	if (Game.spawns['Spawn1'].energy >= 300 && Object.keys(Game.creeps).length < 10) {
-		util.choice([roleHarvester, roleUpgrader]).spawn(Game.spawns['Spawn1']);
+		util.choice([roleHarvester, roleHarvester, roleUpgrader]).spawn(Game.spawns['Spawn1']);
 	}
 	_.forIn(Game.creeps, function(creep, name) {
 		var role = name.split(":")[0]
