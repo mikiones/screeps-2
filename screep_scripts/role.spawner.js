315f06f363a5e446b75ba8b2f5033995ae9ffd9f
Moghedrin <nolat301@gmail.com>
2016-08-22 16:17:35 -0700

    Add function for analyzing energy sources

--- screep_scripts/role.spawner.js
+++ screep_scripts/role.spawner.js
@@ -1,4 +1,15 @@
 var spawner = {
+	anaylze_sources : function(spawner) {
+		var mining_slots = {};
+		var sources = spawner.room.find(FIND_SOURCES);
+		for (source in sources) {
+			source_area = spawner.room.lookAtArea(source.pos.y+1, source.pos.x-1, source.pos.y-1, source.pos.x+1, true);
+			mining_slots[source.id].slots = _.filter(source_area, (obj) => obj.type == 'terrain' && obj.terrain != 'wall').length;
+			mining_slots[source.id].path_from_spawner = spawner.pos.findPathTo(source.pos, {ignoreCreeps : true, ignoreRoads : true})
+		}
+		return mining_slots;
+	},
+	spawner.memory.source_info = mining_slots;
 }
 
 module.exports = spawner;
