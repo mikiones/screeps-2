66d0d7c4e9f876de2f5dcf8325c1323c40c7fc77
Moghedrin <nolat301@gmail.com>
2016-08-22 12:41:43 -0700

    Randomly pick upgrader or harvester

--- screep_scripts/main.js
+++ screep_scripts/main.js
@@ -1,9 +1,10 @@
 var roleHarvester = require('role.simple_harvester');
 var roleUpgrader = require('role.simple_upgrader');
+var util = require('util');
 
 module.exports.loop = function () {
 	if (Game.spawns['Spawn1'].energy >= 300 && Object.keys(Game.creeps).length < 10) {
-		roleHarvester.spawn(Game.spawns['Spawn1']);
+		util.choice([roleHarvester, roleUpgrader]).spawn(Game.spawns['Spawn1']);
 	}
 	_.forIn(Game.creeps, function(creep, name) {
 		var role = name.split(":")[0]
