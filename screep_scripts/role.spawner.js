8ea0fe33cb1e995a0a43909e8c83b280aff3a026
Moghedrin <nolat301@gmail.com>
2016-08-22 16:31:01 -0700

    Convert for loop to low-dash forIn loop

--- screep_scripts/role.spawner.js
+++ screep_scripts/role.spawner.js
@@ -2,11 +2,12 @@ var roleSpawner = {
 	analyze_sources : function(spawner) {
 		var mining_slots = {};
 		var sources = spawner.room.find(FIND_SOURCES);
-		for (source in sources) {
+		_.forIn(sources, function(source, source_id) {
+			mining_slots[source_id] = {};
 			source_area = spawner.room.lookAtArea(source.pos.y+1, source.pos.x-1, source.pos.y-1, source.pos.x+1, true);
-			mining_slots[source.id].slots = _.filter(source_area, (obj) => obj.type == 'terrain' && obj.terrain != 'wall').length;
-			mining_slots[source.id].path_from_spawner = spawner.pos.findPathTo(source.pos, {ignoreCreeps : true, ignoreRoads : true})
-		}
+			mining_slots[source_id].slots = _.filter(source_area, (obj) => obj.type == 'terrain' && obj.terrain != 'wall').length;
+			mining_slots[source_id].path_from_spawner = spawner.pos.findPathTo(source.pos, {ignoreCreeps : true, ignoreRoads : true})
+		});
 		spawner.memory.source_info = mining_slots;
 	},
 }
