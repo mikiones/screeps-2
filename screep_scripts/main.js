6ee3dcfdfe9b2e0f3e2c42486638f637b891a4a1
Moghedrin <nolat301@gmail.com>
2016-08-22 14:32:23 -0700

    Only keep 1 upgrader active at a time

--- screep_scripts/main.js
+++ screep_scripts/main.js
@@ -4,13 +4,19 @@ var util = require('util');
 
 module.exports.loop = function () {
 	if (Game.spawns['Spawn1'].energy >= 300 && Object.keys(Game.creeps).length < 10) {
-		util.choice([roleHarvester, roleHarvester, roleUpgrader]).spawn(Game.spawns['Spawn1']);
+		if (Game.spawns['Spawn1'].memory.upgrader_active) {
+			roleHarvester.spawn(Game.spawns['Spawn1']);
+		} else {
+			roleUpgrader.spawn(Game.spawns['Spawn1']);
+		}
 	}
+	Game.spawns['Spawn1'].memory.upgrader_active = false;
 	_.forIn(Game.creeps, function(creep, name) {
 		var role = name.split(":")[0]
 		if (role == "HARVESTER") {
 			roleHarvester.run(creep);
 		} else if (role == "UPGRADER") {
+	Game.spawns['Spawn1'].memory.upgrader_active = true;
 			roleUpgrader.run(creep);
 		}
 	});
