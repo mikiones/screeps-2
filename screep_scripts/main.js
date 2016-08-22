7865d3d26744f6d52017a8f18c8f38c48665cab8
Moghedrin <nolat301@gmail.com>
2016-08-22 16:16:55 -0700

    Add var for Spawner1

--- screep_scripts/main.js
+++ screep_scripts/main.js
@@ -3,20 +3,21 @@ var roleUpgrader = require('role.simple_upgrader');
 var util = require('util');
 
 module.exports.loop = function () {
-	if (Game.spawns['Spawn1'].energy >= 300 && Object.keys(Game.creeps).length < 10) {
-		if (Game.spawns['Spawn1'].memory.upgrader_active) {
-			roleHarvester.spawn(Game.spawns['Spawn1']);
+	var spawner = Game.spawns['Spawn1'];
+	if (spawner.energy >= 300 && Object.keys(Game.creeps).length < 10) {
+		if (spawner.memory.upgrader_active) {
+			roleHarvester.spawn(spawner);
 		} else {
-			roleUpgrader.spawn(Game.spawns['Spawn1']);
+			roleUpgrader.spawn(spawner);
 		}
 	}
-	Game.spawns['Spawn1'].memory.upgrader_active = false;
+	spawner.memory.upgrader_active = false;
 	_.forIn(Game.creeps, function(creep, name) {
 		var role = name.split(":")[0]
 		if (role == "HARVESTER") {
 			roleHarvester.run(creep);
 		} else if (role == "UPGRADER") {
-			Game.spawns['Spawn1'].memory.upgrader_active = true;
+			spawner.memory.upgrader_active = true;
 			roleUpgrader.run(creep);
 		}
 	});
