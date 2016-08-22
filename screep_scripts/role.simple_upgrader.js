0b51fe6466682b454ca398d13343ab2b33e02b3d
Moghedrin <nolat301@gmail.com>
2016-08-22 12:52:14 -0700

    Update upgrader to have smarter energy mining than harvesters

--- screep_scripts/role.simple_upgrader.js
+++ screep_scripts/role.simple_upgrader.js
@@ -3,15 +3,23 @@ var util = require('util');
 var roleUpgrader = {
 	/** @param {Creep} creep **/
 	run: function(creep) {
-		if(creep.carry.energy == 0) {
-			var sources = creep.room.find(FIND_SOURCES);
-			if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-				creep.moveTo(sources[0]);
+		if(creep.carry.energy < creep.carryCapacity) {
+			var harvest_src = creep.memory.harvest_src;
+			if (!harvest_src) {
+				harvest_src = creep.pos.findClosestByPath(FIND_SOURCES);
+				creep.memory.harvest_src = harvest_src.id;
+			} else {
+				harvest_src = Game.getObjectById(harvest_src);
+			}
+			if(creep.harvest(harvest_src) == ERR_NOT_IN_RANGE) {
+				creep.moveTo(harvest_src);
 			}
 		}
 		else {
 			if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
 				creep.moveTo(creep.room.controller);
+			} else {
+				creep.memory.harvest_src = undefined;
 			}
 		}
 	},
