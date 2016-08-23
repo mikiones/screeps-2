7352b0ee9c5ab8a1cba7d5eeee9449183ec02241
Moghedrin <nolat301@gmail.com>
2016-08-23 16:06:15 -0700

    modify behavior for creep numbers to be more fluid

--- screep_scripts/main.js
+++ screep_scripts/main.js
@@ -5,15 +5,18 @@ var roleSpawner = require('role.spawner');
 var util = require('util');
 var tasks = require('tasks');
 
+var min_creeps = 7;
+var max_creeps = 15;
+
 function new_work(creep) {
 	var spawner = Game.spawns['Spawn1'];
 	var active_creeps = Object.keys(Game.creeps).length;
-	if (active_creeps < 3) {
+	if (active_creeps < min_creeps/2) {
 		creep.memory.cmd = tasks.tasks.FILL.make_cmd(Game.spawns['Spawn1'].id, {store_type : 'energy'});
 	} else if (!spawner.memory.upgrader || !Game.getObjectById(spawner.memory.upgrader)) {
 		spawner.memory.upgrader = creep.id;
 		creep.memory.cmd = tasks.tasks.UPGRADE
-	} else if (active_creeps < 7) {
+	} else if (active_creeps < min_creeps) {
 		creep.memory.cmd = tasks.tasks.FILL.make_cmd(Game.spawns['Spawn1'].id, {store_type : 'energy'});
 	} else {
 		var target_build = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
@@ -28,7 +31,7 @@ function new_work(creep) {
 module.exports.loop = function () {
 	var spawner = Game.spawns['Spawn1'];
 	roleSpawner.init(spawner);
-	if (spawner.energy >= 300 && Object.keys(Game.creeps).length < 5) {
+	if (spawner.energy >= 300 && Object.keys(Game.creeps).length < max_creeps) {
 		roleWorker.spawn(spawner);
 	}
 	_.forIn(Game.creeps, function(creep, name) {
