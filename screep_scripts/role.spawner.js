121729cf04e10f0bd7e7b3437f0f549fe99275da
Moghedrin <nolat301@gmail.com>
2016-08-23 16:41:46 -0700

    Break path to source path to spawn and rc into different ticks

--- screep_scripts/role.spawner.js
+++ screep_scripts/role.spawner.js
@@ -1,11 +1,14 @@
 var roleSpawner = {
 	init : function(spawner) {
-		if (spawner.memory.init) {
+		if (spawner.memory.sources_init && spawner.memory.rc_init) {
 			return;
+		} else if (!spawner.memory.sources_init) {
+			roleSpawner.analyze_sources(spawner);
+			spawner.memory.sources_init = true;
+		} else if (!spawner.memory.rc_init) {
+			roleSpawner.analyze_rc(spawner);
+			spawner.memory.rc_init = true;
 		}
-		roleSpawner.analyze_sources(spawner);
-		roleSpawner.analyze_rc(spawner);
-		spawner.memory.init = true;
 	},
 	add_construction_path : function(src, dst) {
 		var path = src.pos.findPathTo(dst.pos, {ignoreCreeps : true, ignoreRoads : true});
@@ -21,12 +24,15 @@ var roleSpawner = {
 			var source_area = spawner.room.lookAtArea(source.pos.y+1, source.pos.x-1, source.pos.y-1, source.pos.x+1, true);
 			mining_slots[source_id].slots = _.filter(source_area, (obj) => obj.type == 'terrain' && obj.terrain != 'wall').length;
 			roleSpawner.add_construction_path(spawner, source);
-			roleSpawner.add_construction_path(spawner.room.controller, source);
 		});
 		spawner.memory.source_info = mining_slots;
 	},
 	analyze_rc : function(spawner) {
 		roleSpawner.add_construction_path(spawner, spawner.room.controller);
+		var sources = spawner.room.find(FIND_SOURCES);
+		_.forIn(sources, function(source, source_id) {
+			roleSpawner.add_construction_path(spawner.room.controller, source);
+		});
 	},
 }
 
