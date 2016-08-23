06e7a5118783f2783dd2efa3d8b4e19e664b1344
Moghedrin <nolat301@gmail.com>
2016-08-23 13:37:48 -0700

    Added test_run logic

--- screep_scripts/main.js
+++ screep_scripts/main.js
@@ -5,8 +5,19 @@ var util = require('util');
 
 var test_run = false;
 
+function new_work(creep) {
+	creep.memory.cmd = tasks.tasks.FILL.make_cmd(Game.spawns['Spawn1'].id, {store_type : 'energy'});
+}
+
 if (test_run) {
 	module.exports.loop = function () {
+		var spawner = Game.spawns['Spawn1'];
+		if (spawner.energy >= 300 && Object.keys(Game.creeps).length < 5) {
+			roleWorker.spawn(spawner);
+		}
+		_.forIn(Game.creeps, function(creep, name) {
+			roleWorker.run(creep, new_work);
+		});
 	};
 } else {
 	module.exports.loop = function () {
