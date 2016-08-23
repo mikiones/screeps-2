f452fc15eec4f179cc36e26af62fb74891f9e01a
Moghedrin <nolat301@gmail.com>
2016-08-23 11:11:56 -0700

    Added switch for test runs in main

--- screep_scripts/main.js
+++ screep_scripts/main.js
@@ -3,26 +3,31 @@ var roleUpgrader = require('role.simple_upgrader');
 var roleSpawner = require('role.spawner');
 var util = require('util');
 
-module.exports.loop = function () {
-	var spawner = Game.spawns['Spawn1'];
-	if (!spawner.memory.mining_info) {
-		roleSpawner.analyze_sources(spawner);
-	}
-	if (spawner.energy >= 300 && Object.keys(Game.creeps).length < 10) {
-		if (spawner.memory.upgrader_active) {
-			roleHarvester.spawn(spawner);
-		} else {
-			roleUpgrader.spawn(spawner);
+var test_run = false;
+
+if (test_run) {
+} else {
+	module.exports.loop = function () {
+		var spawner = Game.spawns['Spawn1'];
+		if (!spawner.memory.mining_info) {
+			roleSpawner.analyze_sources(spawner);
 		}
-	}
-	spawner.memory.upgrader_active = false;
-	_.forIn(Game.creeps, function(creep, name) {
-		var role = name.split(":")[0]
-		if (role == "HARVESTER") {
-			roleHarvester.run(creep);
-		} else if (role == "UPGRADER") {
-			spawner.memory.upgrader_active = true;
-			roleUpgrader.run(creep);
+		if (spawner.energy >= 300 && Object.keys(Game.creeps).length < 10) {
+			if (spawner.memory.upgrader_active) {
+				roleHarvester.spawn(spawner);
+			} else {
+				roleUpgrader.spawn(spawner);
+			}
 		}
-	});
+		spawner.memory.upgrader_active = false;
+		_.forIn(Game.creeps, function(creep, name) {
+			var role = name.split(":")[0]
+			if (role == "HARVESTER") {
+				roleHarvester.run(creep);
+			} else if (role == "UPGRADER") {
+				spawner.memory.upgrader_active = true;
+				roleUpgrader.run(creep);
+			}
+		});
+	};
 }
