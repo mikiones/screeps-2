102d346ad1e5bbd2a76bd435308bb16632cbbb2c
Moghedrin <nolat301@gmail.com>
2016-08-23 13:41:52 -0700

    Declare var

--- screep_scripts/role.spawner.js
+++ screep_scripts/role.spawner.js
@@ -4,7 +4,7 @@ var roleSpawner = {
 		var sources = spawner.room.find(FIND_SOURCES);
 		_.forIn(sources, function(source, source_id) {
 			mining_slots[source_id] = {};
-			source_area = spawner.room.lookAtArea(source.pos.y+1, source.pos.x-1, source.pos.y-1, source.pos.x+1, true);
+			var source_area = spawner.room.lookAtArea(source.pos.y+1, source.pos.x-1, source.pos.y-1, source.pos.x+1, true);
 			mining_slots[source_id].slots = _.filter(source_area, (obj) => obj.type == 'terrain' && obj.terrain != 'wall').length;
 			mining_slots[source_id].path_from_spawner = spawner.pos.findPathTo(source.pos, {ignoreCreeps : true, ignoreRoads : true})
 		});
