3799ba0c83055c0b4a6e03a5533a94da90f92585
Moghedrin <nolat301@gmail.com>
2016-08-24 10:49:42 -0700

    Begin new work assignment process

--- screep_scripts/role.spawner.js
+++ screep_scripts/role.spawner.js
@@ -4,6 +4,12 @@ var roleWorker = require('role.generic_worker');
 var min_creeps = 7;
 var max_creeps = 15;
 
+var ASSIGNED = 'ASSIGNED';
+var UNASSIGNED = 'UNASSIGNED';
+var SPAWN_MODE = 'SPAWN_MODE';
+var UPGRADE_MODE = 'UPGRADE_MODE';
+var NORMAL_MODE = 'NORMAL_MODE';
+
 var roleSpawner = {
 	init : function(spawner) {
 		if (spawner.memory.sources_init && spawner.memory.rc_init) {
@@ -14,6 +20,9 @@ var roleSpawner = {
 		} else if (!spawner.memory.rc_init) {
 			roleSpawner.analyze_rc(spawner);
 			spawner.memory.rc_init = true;
+		} else if (!spawner.memory.tasks_init) {
+			roleSpawner.initialize_tasks(spawner);
+			spawner.memory.tasks_init = true;
 		}
 	},
 	add_construction_path : function(src, dst) {
@@ -39,12 +48,16 @@ var roleSpawner = {
 		//_.forIn(sources, function(source, source_id) {
 		//	roleSpawner.add_construction_path(spawner.room.controller, source);
 		//});
+	}
+	initialize_tasks : function(spawner) {
+		spawner.memory.task_list = {}; // {target_id : [{cmd, args, status}]
+		spawner.memory.task_mode = SPAWN_MODE;
 	},
 	assign_work : function(spawner, creep) {
 		var active_creeps = Object.keys(Game.creeps).length;
 		if (active_creeps < min_creeps/2) {
 			creep.memory.cmd = tasks.tasks.FILL.make_cmd(Game.spawns['Spawn1'].id, {store_type : 'energy'});
-		} else if (!spawner.memory.upgrader || !Game.getObjectById(spawner.memory.upgrader)) {
+		} else if (!spawner.memory.upgrader || !Game.getObjectById(spawner.memory.upgrader) || creep.id == spawner.memory.upgrader) {
 			spawner.memory.upgrader = creep.id;
 			creep.memory.cmd = tasks.tasks.UPGRADE.make_cmd(creep.room.controller.id, {});
 		} else if (active_creeps < min_creeps) {
@@ -56,6 +69,12 @@ var roleSpawner = {
 			} else {
 				creep.memory.cmd = tasks.tasks.UPGRADE.make_cmd(creep.room.controller.id, {});
 			}
+		}
+	},
+	assign_work_2 : function(spawner, creep) {
+		if (spawner.memory.task_mode == SPAWN_MODE) {
+			creep.memory.cmd = tasks.tasks.FILL.make_cmd(spawner.id, {store_type : 'energy'});
+		} else if (spawner.memory.task_mode == UPGRADE_MODE) {
 		}
 	},
 	spawn_creep : function(spawner) {
