9ef2ffb5f7a2236e45df0daf49a6a74dc795988c
Moghedrin <nolat301@gmail.com>
2016-08-25 16:53:19 -0700

    Made upgrader behavior

--- screep_scripts/behavior.builder.js
+++ screep_scripts/behavior.builder.js
@@ -6,6 +6,12 @@ var builder_behavior = new sm.energy_tasker('builder',
 	base.expend_to.build_nearest_site
 );
 
+var upgrader_behavior = new sm.energy_taske('upgrader',
+	base.chain_handlers(base.withdraw_from.nearest_dropped_energy, base.withdraw_from.nearest_container),
+	base.expend_to.upgrade_nearest_rc
+);
+
 module.exports = {
 	build : builder_behavior,
+	upgrade : upgrader_behavior,
 }
