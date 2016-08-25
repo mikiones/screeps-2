852216ff5c117c05061c1d2aee2c5dd9ef447a78
Moghedrin <nolat301@gmail.com>
2016-08-25 13:54:24 -0700

    Added builder spawning to spawner behavior

--- screep_scripts/behavior.spawner.js
+++ screep_scripts/behavior.spawner.js
@@ -2,18 +2,29 @@ var sm = require('state_machine');
 var util = require('util');
 
 var creep_machine = new sm.state_machine({
-	'BUILD_HARVESTER' : [{state_p : 'RENEW_ALL', cond : (actor, state) => actor.room.find(FIND_MY_CREEPS).length > 7}],
+	'BUILD_HARVESTER' : [{state_p : 'BUILD_BUILDER', cond : (actor, state) => actor.memory.harvesters && actor.memory.harvesters >= 8}],
+	'BUILD_BUILDER' : [{state_p : 'RENEW_ALL', cond : (actor, state) => actor.memory.builders && actor.memory.builders >= 4}],
 	'RENEW_ALL' : [{state_p : 'BUILD_HARVESTER', cond : (actor, state) => actor.room.find(FIND_MY_CREEPS). length <= 7}],
 });
 
 var spawner_behavior = new sm.behavior('spawner', creep_machine, 'BUILD_HARVESTER', {
 	'BUILD_HARVESTER' : function(actor, state) {
-		if (actor.energy >= 200) {
-			var id = 'HARVESTER'.concat(util.make_id());
-			if (actor.createCreep([WORK, CARRY, MOVE], id) == OK) {
+		if (actor.energy >= 300) {
+			var id = 'HARVESTER:'.concat(util.make_id());
+			if (actor.createCreep([WORK, WORK, CARRY, MOVE], id) == OK) {
 				Game.memory.creeps[id] = {};
 			}
 		}
+		actor.memory.harvesters = _.size(actor.room.find(FIND_MY_CREEPS, {filter : (creep) => creep.name.split(':') == 'HARVESTER'}));
+	},
+	'BUILD_BUILDER' : function(actor, state) {
+		if (actor.energy >= 200) {
+			var id = 'BUILDER:'.concat(util.make_id());
+			if (actor.createCreep([WORK, CARRY, MOVE, MOVE, MOVE], id) == OK) {
+				Game.memory.creeps[id] = {};
+			}
+		}
+		actor.memory.builders = _.size(actor.room.find(FIND_MY_CREEPS, {filter : (creep) => creep.name.split(':') == 'BUILDER'}));
 	},
 	'RENEW_ALL' : function(actor, state) {
 		_.forEach(actor.room.find(FIND_MY_CREEPS), actor.renew);
--- screep_scripts/main.js
+++ screep_scripts/main.js
@@ -1,11 +1,22 @@
 var roleWorker = require('role.generic_worker');
 var roleSpawner = require('role.spawner');
 var behavior_harvest = require('behavior.harvest');
+var behavior_builder = require('behavior.builder');
 var behavior_spawner = require('behavior.spawner');
+
+var behaviors = {
+	'BUILDER' : behavior_builder.build,
+	'HARVESTER' : behavior_harvest.harvest,
+};
+
+function run(creep) {
+	var type = creep.name.split(':');
+	behaviors[type].run(creep);
+}
 
 module.exports.loop = function () {
 	_.forEach(Game.creeps, (creep) => behavior_harvest.harvest.run(creep));
-	_.forEach(Game.spawns, (spawn) => behavior_spawner.spawner.run(spawn));
+	_.forEach(Game.spawns, run);
 	//var spawner = Game.spawns['Spawn1'];
 	//roleSpawner.init(spawner);
 	//roleSpawner.spawn_creep(spawner);
