4dff88c577768b76bfd1a386d2c199b36dbf22d4
Moghedrin <nolat301@gmail.com>
2016-08-25 16:59:41 -0700

    Fixed typo

--- screep_scripts/behavior.builder.js
+++ screep_scripts/behavior.builder.js
@@ -6,7 +6,7 @@ var builder_behavior = sm.energy_tasker('builder',
 	base.expend_to.build_nearest_site
 );
 
-var upgrader_behavior = sm.energy_taske('upgrader',
+var upgrader_behavior = sm.energy_tasker('upgrader',
 	base.chain_handlers(base.withdraw_from.nearest_dropped_energy, base.withdraw_from.nearest_container),
 	base.expend_to.upgrade_nearest_rc
 );
