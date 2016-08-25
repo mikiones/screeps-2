e227989bdc284546480faa9a960c9653824c7385
Moghedrin <nolat301@gmail.com>
2016-08-25 14:55:18 -0700

    Modify behavior.builder to use utility functions of behavior.base

---
+++ screep_scripts/behavior.base.js
@@ -0,0 +1,28 @@
+function move_action_on_target(actor, action, target) {
+	if (action(target) == ERR_NOT_IN_RANGE) {
+		actor.moveTo(target);
+	}
+};
+
+var withdraw_from = {
+	nearest : function(actor, action, type, cond) {
+		var target = actor.pos.findClosestByPath(type, { filter : cond });
+		if (target) {
+			move_action_on_target(actor, action, target);
+			return true;
+		}
+		return false;
+	},
+	nearest_container : (actor) => withdraw_from.nearest(actor, actor.withdraw, FIND_STRUCTURES, (struct) => struct.store.energy > 0),
+	nearest_source : (actor) => withdraw_from.nearest(actor, actor.harvest, FIND_SOURCES, (struct) => true),
+	nearest_dropped_energy : (actor) => withdraw_from.nearest(actor, actor.pickup, FIND_DROPPED_ENERGY, (struct) => true),
+};
+
+function chain_state_handlers(handlers) {
+	return (actor, state) => _.find(handlers, (handle) => handle(actor));
+}
+
+module.exports = {
+	chain_handlers : chain_state_handlers,
+	withdraw_from : withdraw_from,
+};
--- screep_scripts/behavior.builder.js
+++ screep_scripts/behavior.builder.js
@@ -1,23 +1,8 @@
 var sm = require('state_machine');
+var base = require('behavior.base');
 
 var builder_behavior = new sm.energy_tasker('builder',
-	function(actor, state) {
-		var containers = actor.room.find(FIND_STRUCTURES, {
-			filter : (struct) => struct.structureType == STRUCTURE_CONTAINER && struct.store.energy > 0,
-		});
-		if (_.size(containers) > 0) {
-			var target = containers[0];
-			if (actor.withdraw(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-				actor.moveTo(target);
-			}
-		} else {
-			//Scavenge
-			var target = actor.pos.findClosestByPath(FIND_DROPPED_ENERGY);
-			if (target && actor.pickup(target) == ERR_NOT_IN_RANGE) {
-				actor.moveTo(target);
-			}
-		}
-	},
+	base.chain_handlers([base.withdraw_from.nearest_dropped_energy, base.withdraw_from.nearest_container]),
 	function(actor, state) {
 		var target_build = actor.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
 		if (target_build && actor.build(target_build) == ERR_NOT_IN_RANGE) {
