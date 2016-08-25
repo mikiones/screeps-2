ffd037ac2043da11b8f8ed82f52c0f62f833493e
Moghedrin <nolat301@gmail.com>
2016-08-25 13:11:31 -0700

    Initial behavior.builder implementation

---
+++ screep_scripts/behavior.builder.js
@@ -0,0 +1,22 @@
+var sm = require('state_machine');
+
+var builder_behavior = new sm.energy_tasker('builder',
+	function(actor, state) {
+		var containers = actor.room.find(FIND_STRUCTURES);
+		if (_.size(containers) > 0) {
+		} else {
+			//Scavenge
+			var droppedEnergy = actor.room.find(FIND_DROPPED_ENERGY);
+			var target = _.minBy(droppedEnergy, _.flow([actor.pos.findPath, _.size]));
+			if (actor.pickup(target) == ERR_NOT_IN_RANGE) {
+				actor.moveTo(target);
+			}
+		}
+	},
+	function(actor, state) {
+		var target_build = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
+		if (target_build && actor.build(target_build) == ERR_NOT_IN_RANGE) {
+			actor.moveTo(target_build);
+		}
+	}
+);
