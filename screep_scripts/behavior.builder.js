6f7bcf0d9a1d681c00ddc31ca34d2fc9897a4cf2
Moghedrin <nolat301@gmail.com>
2016-08-25 16:59:04 -0700

    Fix from energy_tasker remake

--- screep_scripts/behavior.builder.js
+++ screep_scripts/behavior.builder.js
@@ -1,12 +1,12 @@
 var sm = require('state_machine');
 var base = require('behavior.base');
 
-var builder_behavior = new sm.energy_tasker('builder',
+var builder_behavior = sm.energy_tasker('builder',
 	base.chain_handlers(base.withdraw_from.nearest_dropped_energy, base.withdraw_from.nearest_container),
 	base.expend_to.build_nearest_site
 );
 
-var upgrader_behavior = new sm.energy_taske('upgrader',
+var upgrader_behavior = sm.energy_taske('upgrader',
 	base.chain_handlers(base.withdraw_from.nearest_dropped_energy, base.withdraw_from.nearest_container),
 	base.expend_to.upgrade_nearest_rc
 );
--- screep_scripts/behavior.harvest.js
+++ screep_scripts/behavior.harvest.js
@@ -1,7 +1,7 @@
 var sm = require('state_machine');
 var base = require('behavior.base');
 
-var harvest_behavior = new sm.energy_tasker('harvest',
+var harvest_behavior = sm.energy_tasker('harvest',
 	base.withdraw_from.nearest_source,
 	base.chain_handlers(base.expend_to.transfer_nearest_container,
 		base.expend_to.transfer_nearest_spawn, base.expend_to.transfer_spawn_ground)
