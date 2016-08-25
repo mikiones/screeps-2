858e37546b61357b4cd430fe48a8242f5562ffd4
Moghedrin <nolat301@gmail.com>
2016-08-25 11:35:34 -0700

    Fix main to run spawner behavior on all spawners

--- screep_scripts/behavior.spawner.js
+++ screep_scripts/behavior.spawner.js
@@ -1,11 +1,11 @@
 var sm = require('state_machine');
 
-var create_creep_machine = new sm.state_machine({
+var creep_machine = new sm.state_machine({
 	'BUILD_HARVESTER' : [{state_p : 'RENEW_ALL', cond : (actor, state) => actor.room.find(FIND_MY_CREEPS).length > 7}],
 	'RENEW_ALL' : [{state_p : 'BUILD_HARVESTER', cond : (actor, state) => actor.room.find(FIND_MY_CREEPS). length <= 7}],
 });
 
-var spawner_behavior = new sm.behavior('spawner', sm.create_creep_machine, 'BUILD_HARVESTER', {
+var spawner_behavior = new sm.behavior('spawner', creep_machine, 'BUILD_HARVESTER', {
 	'BUILD_HARVESTER' : function(actor, state) {
 		if (actor.energy >= 200) {
 			actor.createCreep([WORK, CARRY, MOVE], _.uniqueId('HARVESTER'));
--- screep_scripts/main.js
+++ screep_scripts/main.js
@@ -1,12 +1,11 @@
 var roleWorker = require('role.generic_worker');
 var roleSpawner = require('role.spawner');
 var behavior_harvest = require('behavior.harvest');
+var behavior_spawner = require('behavior.spawner');
 
 module.exports.loop = function () {
-	Game.spawns['Spawn1'].createCreep([WORK, CARRY, MOVE], _.uniqueId());
-	_.forEach(Game.creeps, function(creep) {
-		behavior_harvest.harvest.run(creep);
-	});
+	_.forEach(Game.creeps, behavior_harvest.harvest.run);
+	_.forEach(Game.spawner, behavior_spawner.spawner.run);
 	//var spawner = Game.spawns['Spawn1'];
 	//roleSpawner.init(spawner);
 	//roleSpawner.spawn_creep(spawner);
