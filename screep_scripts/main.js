a2ab1d5d8da5a5ad63d1f8a7ae1bc02ca3c09b2a
Moghedrin <nolat301@gmail.com>
2016-08-25 11:40:00 -0700

    Fix forEach functions

--- screep_scripts/main.js
+++ screep_scripts/main.js
@@ -4,8 +4,8 @@ var behavior_harvest = require('behavior.harvest');
 var behavior_spawner = require('behavior.spawner');
 
 module.exports.loop = function () {
-	_.forEach(Game.creeps, behavior_harvest.harvest.run);
-	_.forEach(Game.spawner, behavior_spawner.spawner.run);
+	_.forEach(Game.creeps, (creep) => behavior_harvest.harvest.run(creep));
+	_.forEach(Game.spawns, (spawn) => behavior_spawner.spawner.run(spawn));
 	//var spawner = Game.spawns['Spawn1'];
 	//roleSpawner.init(spawner);
 	//roleSpawner.spawn_creep(spawner);
