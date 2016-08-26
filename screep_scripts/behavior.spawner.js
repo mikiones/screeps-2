d711f3f6424233a9e4c6e3c549832926ba59d2db
Moghedrin <nolat301@gmail.com>
2016-08-25 19:44:56 -0700

    Remove commented out code

--- screep_scripts/behavior.spawner.js
+++ screep_scripts/behavior.spawner.js
@@ -33,55 +33,6 @@ var behavior = {
 	},
 };
 
-//var creep_machine = new sm.state_machine({
-//	'BUILD_HARVESTER' : [{state_p : 'BUILD_UPGRADER', cond : (actor, state) => actor.memory.harvesters && actor.memory.harvesters >= 8}],
-//	'BUILD_UPGRADER' : [{state_p : 'BUILD_BUILDER', cond : (actor, state) => actor.memory.upgraders && actor.memory.upgraders >= 1}],
-//	'BUILD_BUILDER' : [
-//		{state_p : 'RENEW_ALL', cond : (actor, state) => actor.memory.builders && actor.memory.builders >= 4},
-//		{state_p : 'BUILD_HARVESTER', cond : (actor, state) => actor.memory.harvesters < 8},
-//	],
-//	'RENEW_ALL' : [{state_p : 'BUILD_HARVESTER', cond : (actor, state) => actor.room.find(FIND_MY_CREEPS).length < 8}],
-//});
-//
-//var spawner_behavior = new sm.behavior('spawner', creep_machine, 'BUILD_HARVESTER', {
-//	'BUILD_HARVESTER' : function(actor, state) {
-//		if (actor.energy >= 300) {
-//			var id = 'HARVESTER:'.concat(util.make_id());
-//			if (actor.createCreep([WORK, WORK, CARRY, MOVE], id) == OK) {
-//				Game.memory.creeps[id] = {};
-//			}
-//		}
-//		actor.memory.harvesters = _.size(actor.room.find(FIND_MY_CREEPS, {filter : (creep) => creep.name.split(':')[0] == 'HARVESTER'}));
-//		actor.memory.builders = _.size(actor.room.find(FIND_MY_CREEPS, {filter : (creep) => creep.name.split(':')[0] == 'BUILDER'}));
-//		actor.memory.upgraders = _.size(actor.room.find(FIND_MY_CREEPS, {filter : (creep) => creep.name.split(':')[0] == 'UPGRADER'}));
-//	},
-//	'BUILD_UPGRADER' : function(actor, state) {
-//		if (actor.energy >= 300) {
-//			var id = 'UPGRADER:'.concat(util.make_id());
-//			if (actor.createCreep([WORK, CARRY, MOVE, MOVE, MOVE], id) == OK) {
-//				Game.memory.creeps[id] = {};
-//			}
-//		}
-//		actor.memory.harvesters = _.size(actor.room.find(FIND_MY_CREEPS, {filter : (creep) => creep.name.split(':')[0] == 'HARVESTER'}));
-//		actor.memory.builders = _.size(actor.room.find(FIND_MY_CREEPS, {filter : (creep) => creep.name.split(':')[0] == 'BUILDER'}));
-//		actor.memory.upgraders = _.size(actor.room.find(FIND_MY_CREEPS, {filter : (creep) => creep.name.split(':')[0] == 'UPGRADER'}));
-//	},
-//	'BUILD_BUILDER' : function(actor, state) {
-//		if (actor.energy >= 300) {
-//			var id = 'BUILDER:'.concat(util.make_id());
-//			if (actor.createCreep([WORK, CARRY, MOVE, MOVE, MOVE], id) == OK) {
-//				Game.memory.creeps[id] = {};
-//			}
-//		}
-//		actor.memory.harvesters = _.size(actor.room.find(FIND_MY_CREEPS, {filter : (creep) => creep.name.split(':')[0] == 'HARVESTER'}));
-//		actor.memory.builders = _.size(actor.room.find(FIND_MY_CREEPS, {filter : (creep) => creep.name.split(':')[0] == 'BUILDER'}));
-//		actor.memory.upgraders = _.size(actor.room.find(FIND_MY_CREEPS, {filter : (creep) => creep.name.split(':')[0] == 'UPGRADER'}));
-//	},
-//	'RENEW_ALL' : function(actor, state) {
-//		_.forEach(actor.room.find(FIND_MY_CREEPS), actor.renew);
-//	},
-//});
-
 module.exports = {
 	behavior : behavior,
 }
