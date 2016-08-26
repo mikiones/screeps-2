3c403a33729a5097d510b8084e351721ac7a074e
Moghedrin <nolat301@gmail.com>
2016-08-26 09:55:59 -0700

    Harvesters now give to spawn if there are only harvesters

--- screep_scripts/behavior.harvest.js
+++ screep_scripts/behavior.harvest.js
@@ -1,11 +1,17 @@
 var sm = require('state_machine');
 var base = require('behavior.base');
 
-var harvest_behavior = sm.energy_tasker('harvest',
-	base.withdraw_from.nearest_source,
-	base.chain_handlers(base.expend_to.transfer_nearest_container,
-		base.expend_to.transfer_nearest_spawn, base.expend_to.transfer_spawn_ground)
-);
+function harvest_transfer_behavior(actor) {
+	var count = _.size(actor.room.find(FIND_MY_CREEPS, {filter : (creep) => creep.name.split(':')[0] == 'harvester'}));
+	if (count >= _.size(Game.creeps)) {
+		return base.expend_to.transfer_nearest_spawn(actor);
+	}
+	return base.chain_handlers(base.expend_to.transfer_nearest_container,
+		base.expend_to.transfer_nearest_spawn, base.expend_to.transfer_spawn_ground)(actor);
+
+}
+
+var harvest_behavior = sm.energy_tasker('harvest', base.withdraw_from.nearest_source, harvest_transfer_behavior);
 var harvester_creep_type = new base.creep_type('harvester', harvest_behavior,
 	[
 		[WORK, WORK, CARRY, MOVE],
