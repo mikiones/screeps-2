bd7bde33f67657e2b3cc46e4769816a531fda7b5
Moghedrin <nolat301@gmail.com>
2016-08-31 16:14:56 -0700

    Automatically remove dead creep's memory

--- screep_scripts/main.js
+++ screep_scripts/main.js
@@ -3,6 +3,15 @@ require('extended_functionality');
 var harvester_behavior = require('behavior.harvesters');
 var upgrader_behavior = require('behavior.upgraders');
 
+function cleanCreepMemory() {
+	_.forEach(Memory.creeps, function(creep_mem, key) {
+		if (!Game.creeps[key]) {
+			Memory.creeps[key] = undefined;
+		}
+	});
+};
+
 module.exports.loop = function() {
+	cleanCreepMemory();
 	_.forEach(_.filter(Game.creeps, {fatigue : 0}), upgrader_behavior.simple);
 }
