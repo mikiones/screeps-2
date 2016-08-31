22f494f5f500a20bb37828b50dd58d1cb54ac691
Moghedrin <nolat301@gmail.com>
2016-08-31 13:29:19 -0700

    Refactor creep nodes

--- screep_scripts/screeps_behaviors.js
+++ screep_scripts/screeps_behaviors.js
@@ -43,46 +43,36 @@ function get_nearest_spawn(context) {
 	var target = context.actor.pos.findClosestByPath(FIND_MY_SPAWNS);
 	return target;
 }
-
 var push_nearest_source = new (push_stack_value(get_nearest_source));
 var push_nearest_spawn = new (push_stack_value(get_nearest_spawn));
 
-var creep_empty_energy = new (btree.builders.context_operation(function(context) {
-	if (context.actor.carry && context.actor.carry.energy == 0) {
+var creep_status = (func) => btree.builders.context_operation(function(context) {
+	if (func(context.actor)) {
 		return btree.SUCCESS;
 	}
 	return btree.FAILURE;
-}));
+});
 
-var creep_not_full_energy = new (btree.builders.context_operation(function(context) {
-	if (context.actor.carry && context.actor.carry.energy < context.actor.carryCapacity) {
+var creep_action_stack = (action) => with_stack_value(function(context, target) {
+	if (context.actor[action](target) == OK) {
 		return btree.SUCCESS;
 	}
 	return btree.FAILURE;
-}));
+});
 
-var creep_harvest_stack = new (with_stack_value(function(context, target) {
-	if (context.actor.harvest) {
-		if (context.actor.harvest(target) == OK) {
-			return btree.SUCCESS;
-		}
+var creep_resource_action_stack = (action, resource_type) => with_stack_value(function(context, target) {
+	if (context.actor[action](target, resource_type) == OK) {
+		return btree.SUCCESS;
 	}
 	return btree.FAILURE;
-}));
+});
 
-var creep_transfer_stack = new (with_stack_value(function(context, target) {
-	if (context.actor.transfer) {
-		if (context.actor.transfer(target, RESOURCE_ENERGY) == OK) {
-			return btree.SUCCESS;
-		}
-	}
-	return btree.FAILURE;
-}));
-
-var creep_move_to_stack = new (with_stack_value(function(context, target) {
-	context.actor.moveTo(target);
-	return btree.SUCCESS;
-}));
+var creep_empty_energy = new (creep_status(creep => creep.carry.energy == 0));
+var creep_not_full_energy = new (creep_status(creep => creep.carry.energy < creep.carryCapacity));
+var creep_harvest_stack = new (creep_action_stack('harvest'));
+var creep_transfer_stack = new (creep_resource_action_stack('transfer', RESOURCE_ENERGY));
+var creep_move_to_stack = new (creep_action_stack('moveTo'));
+var creep_succeeding_move_to_stack = new btree.decorators.always_succeed(creep_move_to_stack);
 
 var creep_drop_energy = new (btree.builders.context_operation(function(context) {
 	context.actor.drop(RESOURCE_ENERGY);
@@ -97,8 +87,8 @@ var next_to_stack = new (with_stack_value(function(context, target) {
 }));
 
 var drop_if_by_stack = new btree.composites.sequence([next_to_stack, creep_drop_energy]);
-var creep_harvest_or_move = new btree.composites.select([creep_harvest_stack, creep_move_to_stack]);
-var creep_transfer_or_move = new btree.composites.select([creep_transfer_stack, drop_if_by_stack, creep_move_to_stack]);
+var creep_harvest_or_move = new btree.composites.select([creep_harvest_stack, creep_succeeding_move_to_stack]);
+var creep_transfer_or_move = new btree.composites.select([creep_transfer_stack, drop_if_by_stack, creep_succeeding_move_to_stack]);
 var creep_harvest_nearest_source = new btree.composites.sequence([creep_not_full_energy, push_nearest_source, creep_harvest_or_move, pop_stack]);
 var creep_transfer_nearest_spawn = new btree.composites.sequence([push_nearest_spawn, creep_transfer_or_move, pop_stack]);
 
