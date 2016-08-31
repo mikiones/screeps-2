557c7e0f296cf8ab4485afe538c9eb8e5ed2a225
Moghedrin <nolat301@gmail.com>
2016-08-31 10:11:31 -0700

    Initial screeps behavior module

--- screep_scripts/behavior_tree.js
+++ screep_scripts/behavior_tree.js
@@ -1,5 +1,4 @@
 var _ = require('lodash');
-//
 
 var SUCCESS = 'SUCCESS';
 var RUNNING = 'RUNNING';
---
+++ screep_scripts/screeps_behaviors.js
@@ -0,0 +1,28 @@
+var btree = require('behavior_tree');
+
+function create_context(actor, game) {
+	return {actor : actor, game : game}
+}
+
+var push_stack_value = func => btree.builders.context_operation((context) => {
+	if (context.stack) {
+		context.stack.push(func(context));
+	} else {
+		context.stack = [func(context)];
+	}
+	return btree.SUCCESS;
+});
+
+var with_stack_value = func => btree.builders.context_operation((context) => {
+	if (context.stack && context.stack.length > 0) {
+		return func(context, context.stack.pop());
+	}
+	return btree.FAILURE;
+});
+
+function get_nearest_source(context) {
+	var target = context.actor.pos.findClosestByPath(FIND_SOURCES);
+	return target.pos;
+}
+
+var push_nearest_source = new (push_stack_value(get_nearest_source));
