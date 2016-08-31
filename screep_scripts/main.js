1d79753e93ba75414be2eb8f8881510140ae7d7e
Moghedrin <nolat301@gmail.com>
2016-08-31 14:22:56 -0700

    Only put through behaviors the ones with a fatigue of 0

--- screep_scripts/main.js
+++ screep_scripts/main.js
@@ -3,5 +3,5 @@ require('extended_functionality');
 var harvester_behavior = require('behavior.harvesters');
 
 module.exports.loop = function() {
-	_.forEach(Game.creeps, harvester_behavior.simple);
+	_.forEach(_.filter(Game.creeps, {fatigue : 0}), harvester_behavior.simple);
 }
