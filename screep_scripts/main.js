d711fe37805778f0e920b0efce741ef825348ee0
Moghedrin <nolat301@gmail.com>
2016-08-31 09:23:12 -0700

    Start over

--- screep_scripts/main.js
+++
@@ -1,6 +0,0 @@
-var spawner = require('behavior.spawner');
-
-module.exports.loop = function () {
-	_.forEach(Game.creeps, spawner.behavior.run_creep);
-	_.forEach(Game.spawns, spawner.behavior.run_spawner);
-};
