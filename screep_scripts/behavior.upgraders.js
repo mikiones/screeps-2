620f05d88bc5ac09cdc103fe0671302848c54915
Moghedrin <nolat301@gmail.com>
2016-09-01 13:52:21 -0700

    Now only fills up if empty

--- screep_scripts/behavior.upgraders.js
+++ screep_scripts/behavior.upgraders.js
@@ -8,7 +8,7 @@ var withdraw_nearest_spawn = new btree.composites.sequence(
 	[sbehave.push_nearest_spawn, withdraw_or_move, sbehave.pop_stack]);
 
 var withdraw_energy = new btree.composites.sequence(
-	[sbehave.creep.not_full_energy, withdraw_nearest_spawn]);
+	[sbehave.creep.empty_energy, withdraw_nearest_spawn]);
 
 var pickup_or_move = new btree.composites.select(
 	[sbehave.creep.pickup_stack, sbehave.creep.succeeding_move_to_stack]);
@@ -17,7 +17,7 @@ var pickup_nearest_dropped_energy = new btree.composites.sequence(
 	[sbehave.push_nearest_dropped_energy, pickup_or_move, sbehave.pop_stack]);
 
 var pickup_dropped_energy = new btree.composites.sequence(
-	[sbehave.creep.not_full_energy, pickup_nearest_dropped_energy, sbehave.push_nearest_spawn, sbehave.creep.succeeding_move_to_stack, sbehave.pop_stack]);
+	[sbehave.creep.empty_energy, pickup_nearest_dropped_energy, withdraw_energy]);
 
 var upgrade_or_move = new btree.composites.select(
 	[sbehave.creep.upgrade_stack, sbehave.creep.succeeding_move_to_stack]);
@@ -26,7 +26,7 @@ var upgrade_room_rc = new btree.composites.sequence(
 	[sbehave.push_room_controller, upgrade_or_move, sbehave.pop_stack]);
 
 var simple_upgrader_behavior = new btree.composites.sequence(
-	[new btree.decorators.inverter(withdraw_energy), upgrade_room_rc]);
+	[new btree.decorators.inverter(pickup_dropped_energy), upgrade_room_rc]);
 
 module.exports = {
 	simple : function(creep) {
