5520db9cba992a9d356171d37f11086b239463ad
Moghedrin <nolat301@gmail.com>
2016-09-01 13:20:56 -0700

    Should return to spawn if no energy on ground

--- screep_scripts/behavior.upgraders.js
+++ screep_scripts/behavior.upgraders.js
@@ -4,11 +4,11 @@ var sbehave = require('screeps_behaviors');
 var pickup_or_move = new btree.composites.select(
 	[sbehave.creep.pickup_stack, sbehave.creep.succeeding_move_to_stack]);
 
-var pickup_nearest_dropped_energy - new btree.composites.sequence(
+var pickup_nearest_dropped_energy = new btree.composites.sequence(
 	[sbehave.push_nearest_dropped_energy, pickup_or_move, sbehave.pop_stack]);
 
 var pickup_dropped_energy = new btree.composites.sequence(
-	[sbehave.creep.not_full_energy, pickup_nearest_dropped_energy]);
+	[sbehave.creep.not_full_energy, pickup_nearest_dropped_energy, sbehave.push_nearest_spawn, sbehave.creep.succeeding_move_to_stack, sbehave.pop_stack]);
 
 var upgrade_or_move = new btree.composites.select(
 	[sbehave.creep.upgrade_stack, sbehave.creep.succeeding_move_to_stack]);
