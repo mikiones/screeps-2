0bbc8a02e704f1b94741800c5b3e7e2026c2b997
Moghedrin <nolat301@gmail.com>
2016-09-01 13:50:00 -0700

    Upgraders withdraw from spawn again

--- screep_scripts/behavior.upgraders.js
+++ screep_scripts/behavior.upgraders.js
@@ -1,6 +1,15 @@
 var btree = require('behavior_tree');
 var sbehave = require('screeps_behaviors');
 
+var withdraw_or_move = new btree.composites.select(
+	[sbehave.creep.withdraw_stack, sbehave.creep.succeeding_move_to_stack]);
+
+var withdraw_nearest_spawn = new btree.composites.sequence(
+	[sbehave.push_nearest_spawn, withdraw_or_move, sbehave.pop_stack]);
+
+var withdraw_energy = new btree.composites.sequence(
+	[sbehave.creep.not_full_energy, withdraw_nearest_spawn]);
+
 var pickup_or_move = new btree.composites.select(
 	[sbehave.creep.pickup_stack, sbehave.creep.succeeding_move_to_stack]);
 
@@ -17,7 +26,7 @@ var upgrade_room_rc = new btree.composites.sequence(
 	[sbehave.push_room_controller, upgrade_or_move, sbehave.pop_stack]);
 
 var simple_upgrader_behavior = new btree.composites.sequence(
-	[new btree.decorators.inverter(pickup_dropped_energy), upgrade_room_rc]);
+	[new btree.decorators.inverter(withdraw_energy), upgrade_room_rc]);
 
 module.exports = {
 	simple : function(creep) {
