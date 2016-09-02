faf2f1fff615e47f5ecd3163000654067c0df466
Moghedrin <nolat301@gmail.com>
2016-09-01 19:06:39 -0700

    Initial harvesters deposit to containers first

--- screep_scripts/behavior.harvesters.js
+++ screep_scripts/behavior.harvesters.js
@@ -33,9 +33,30 @@ var needs_new_container_target = new (sbehave.actor_status(function(creep) {
 	var target = Game.getObjectById(creep.memory.container_target);
 	return !target || !target.structureType || target.structureType != STRUCTURE_CONTAINER || target.store.energy >= target.storeCapacity;
 }));
-
+var transfer_container_target = new (btree.builders.context_operation(function(context) {
+	if (context.actor.memory.container_target) {
+		var target = Game.getObjectById(context.actor.memory.container_target);
+		if (context.actor.transfer(target, RESOURCE_ENERGY) == OK) {
+			return btree.SUCCESS;
+		}
+	}
+	return btree.FAILURE;
+}));
+var move_to_container_target = new (btree.builders.context_operation(function(context) {
+	if (context.actor.memory.container_target) {
+		var target = Game.getObjectById(context.actor.memory.container_target);
+		if (context.actor.moveTo(target) == OK) {
+			return btree.SUCCESS;
+		}
+	}
+	return btree.FAILURE;
+}));
 var set_container_target = new btree.composites.sequence(
-	[needs_new_container_target, set_nearest_nonempty_container_container_target]);
+	[new btree.decorators.always_succeed(needs_new_container_target), set_nearest_nonempty_container_container_target]);
+var transfer_or_move_to_container = new btree.composites.select(
+	[transfer_container_target, new btree.decorators.always_succeed(move_to_container_target)]);
+var transfer_to_container = new btree.composites.sequence(
+	[set_container_target, new btree.decorators.inverter(needs_new_container_target), transfer_or_move_to_container]);
 
 var register_harvester_stack = new (sbehave.with_stack_value(function(context, target) {
 	target.registerHarvester(context.actor);
@@ -65,8 +86,11 @@ var harvest_target_source = new btree.composites.sequence(
 var transfer_nearest_spawn = new btree.composites.sequence(
 	[sbehave.push_nearest_spawn, transfer_or_move, sbehave.pop_stack]);
 
+var transfer_target = new btree.composites.select(
+	[transfer_to_container, transfer_nearest_spawn]);
+
 var simple_harvest_behavior = new btree.composites.sequence(
-	[new btree.decorators.inverter(harvest_target_source), transfer_nearest_spawn]);
+	[new btree.decorators.inverter(harvest_target_source), transfer_target]);
 
 module.exports = {
 	simple : function(creep) {
